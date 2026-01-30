7a9742edc9c2a3e633e9785a234e04def1ac785b
Nishoodan <nishoodank@gmail.com>
2026-01-30 21:03:23 +0530

    completed!!

--- level-2.py
+++ level-2.py
@@ -1,8 +1,8 @@
 #Count words in a sentence
 #"I love Python" → 3
 
-#sentence = str(input("Enter sentence for counting words: ")).split(" ")
-#print(sum(1 for i in sentence))
+sentence = str(input("Enter sentence for counting words: ")).split(" ")
+print(sum(1 for i in sentence))
 
 #----------------------------------------------------------------------------------------#
 
@@ -10,9 +10,9 @@
 #Sum of digits of a number
 #1234 → 10
 num1 = int(input("Enter number for adding/summing: "))
-summ = 0
-l = len(str(num1))
+
 s = str(num1)
+summ = 0
 
 for i in range(len(s)):
     summ += int(s[i])
@@ -23,3 +23,19 @@ print(summ)
 
 #Remove duplicates from a list
 #[1,2,2,3,4,4] → [1,2,3,4]
+
+num2 = set(input("Enter numbers to delete duplicates with comma ',' : "))
+
+garbage = []
+main_list = []
+for i in num2:
+    if i == "," :
+        garbage.append(i)
+    else:
+        main_list.append(i)
+garbage = []
+print(main_list)
+print(garbage)
+
+
+
